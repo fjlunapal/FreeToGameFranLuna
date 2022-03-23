825fa38556f68d92c0870e6c92aef33f8959ccce
flunapal <fjlunapalma@gmail.com>
2022-03-23 09:36:37 +0100

    Pagina de register recoge los datos que se le introducen

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -13,23 +13,39 @@ import { Router } from '@angular/router';
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
 export class RegisterPage implements OnInit {
-  firstName = new FormControl();
-  secondName = new FormControl();
-  email = new FormControl();
-  password = new FormControl();
-  c_password = new FormControl();
-  Validators: any;
 
-  constructor(fb: FormBuilder, public auth: AuthService, public router: Router) { }
+  ionicForm: FormGroup;
+  isSubmitted = false;
+  Validators: any;  
 
+  constructor(public fb: FormBuilder, public auth: AuthService, public router: Router) {   
+    this.ionicForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      secondName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['', [Validators.required]],
+      c_password: ['', [Validators.required]]
+    })
+     }
+  
+  ngOnInit() {
+  }
   async register(){
-    this.auth.register(this.Validators.firstName, this.Validators.lastName, this.Validators.email, this.Validators.password, this.Validators.c_password).then(data =>{
+    this.auth.register(this.Validators.firstName, this.Validators.lastName, this.Validators.email, this.Validators.password, this.Validators.c_password ).then(data =>{
       
     })
   }
 
-  ngOnInit() {
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+    }
   }
 
 }
