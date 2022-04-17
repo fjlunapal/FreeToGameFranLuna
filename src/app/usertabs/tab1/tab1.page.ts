c8d36292388e5cee1295a9f330f51f53d4a0b986
fjlunapal <fjlunapalma@gmail.com>
2022-04-17 21:25:36 +0200

    Funciona en movil

--- src/app/usertabs/tab1/tab1.page.ts
+++ src/app/usertabs/tab1/tab1.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Game } from '../../services/interfaces/Game';
-
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,14 +9,26 @@ import { Game } from '../../services/interfaces/Game';
   styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
-  constructor(private authService: AuthService) { }
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
   games: Game[] = [];
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.authService.getGames().then((games: Game[]) => {
       this.games = games;
       console.log(games)
     });
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      if (this.games.length === 400) {
+        event.target.disabled = true;
+      }
+    }, 500);
   }
 
 }
